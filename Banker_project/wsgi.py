b5de238d1567a4b4a116b90d1d209e933c8f35b8
Pranav3303 <160731119+Pranav3303@users.noreply.github.com>
2025-04-18 13:06:45 +0530

    5

--- Banker_project/wsgi.py
+++ Banker_project/wsgi.py
@@ -11,8 +11,21 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from whitenoise import WhiteNoise
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Banker_project.settings')
 
 application = get_wsgi_application()
 
 app = application
+
+app = WhiteNoise(
+    application,
+    root=os.path.join(os.path.BASE_DIR,"own_repo"),
+    prefix="static/"
+)
+
+application.add_files(
+    os.path.join(os.path.BASE_DIR,"gallery"),
+    prefix="gallery/"
+)
